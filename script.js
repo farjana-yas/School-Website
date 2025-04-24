006f9ae6ce762eef2306c338907304cdb1f70e1a
farjana-yas <farjana119955@gmail.com>
2025-04-24 19:14:08 +0400

    updates

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function showSection(params) {
-    const elements = document.querySelectorAll(".main-content > div") //elements
-    elements.forEach(dt => {
-        dt.classList.add('hidden')
+function showSection(idname){
+    const allObject = document.querySelectorAll(".main-content > div");
+    allObject.forEach((object) => {
+      object.classList.add("hidden");
     })
-    document.getElementById(params + "section").classList.remove("hidden"); // = studentsSection
-}
+    const object = document.getElementById(idname +"Section");
+    object.style.transition = "all 0.5s ease";
+    object.classList.remove("hidden");
+  
+  };
+  
+  
+  
+  function loginPage(){
+    window.location.href = "login.html";
+  }
+  function homepage(){
+    window.location.href = "index.html";
+  }
+  
+  function submitForm() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+      
+  
+      if (username === 'admin' && password === 'password123') {
+          window.location.href = 'index.html';
+      } else {
+          document.getElementById('errorMessage').style.display = 'block';
+      }
+    });
+  }
